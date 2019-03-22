f9cb0d903cd09ed76ee121c6a65ffa72ff97a756
0xJeremy <jeremy.kanovsky@tufts.edu>
2019-03-22 16:33:47 -0400

    Added Graph Visualization

--- find_basis.py
+++ find_basis.py
@@ -1,6 +1,8 @@
 import json
 import networkx as nx
 import time
+import matplotlib as matplotlib
+import matplotlib.pyplot as plt
 
 DICTIONARY = 'small_dictionary.json'
 
@@ -12,18 +14,13 @@ def read_dictionary(file):
 	print("Dictionary Loaded (" + str(end - start) + " sec.)")
 	return data
 
-def main():
-	rawdata = read_dictionary(DICTIONARY)
+def clean_dictionary(rawdata):
 	data = {}
 	for key in rawdata:
 		if '*' not in rawdata[key]:
 			data[key.lower()] = rawdata[key].lower()
 
 	# for key in data:
-	# 	if '*' in data[key]:
-	# 		data.remove(key)
-	# 		continue
-	# 	data[key] = data[key].strip()
 	# 	print("Word: " + str(key))
 	# 	print("Definition: " + str(data[key]))
 
@@ -32,13 +29,9 @@ def main():
 		data[key] = ''.join([char for char in data[key] if char.isalpha() or char == ' ']) # If you look closely, you can see what it's doing.
 		data[key] = data[key].split()
 
-	wordnet = nx.DiGraph()
-	wordnet.add_nodes_from(data.keys())
-	# wordnet.add_node(1)
-	print(wordnet.nodes)
+	return data
 
-	print("Added word nodes to graph.")
-
+def populate_graph(data, wordnet):
 	start_time = time.time()
 	for key in data:
 		targets = data[key]
@@ -50,9 +43,26 @@ def main():
 				print("could not add edge from " + str(key) + " to " + str(target))
 	end_time = time.time()
 	print("Graph populated. (" + str(end_time - start_time) + " sec.)")
+	return wordnet
 
-	print(wordnet.edges)
+def generate_image(fgraph):
+	pos = nx.layout.spring_layout(fgraph)
+	nodes = nx.draw_networkx_nodes(fgraph, pos, node_color='blue', node_sizes = [1] * len(fgraph.nodes))
+	edges = nx.draw_networkx_edges(fgraph, pos, arrowstyle='->',   node_sizes = [1] * len(fgraph.nodes),
+									arrowsize=5, edge_cmap=plt.cm.Blues, width=1)
 
+	plt.show()
+
+def main():
+	raw_dictionary = read_dictionary(DICTIONARY)
+	data = clean_dictionary(raw_dictionary)
+
+	wordnet = nx.DiGraph()
+	wordnet.add_nodes_from(data.keys())
+
+	wordnet = populate_graph(data, wordnet)
+
+	generate_image(wordnet)
 
 if __name__ == '__main__':
 	main()
